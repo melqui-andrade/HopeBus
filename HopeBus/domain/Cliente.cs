cbca50a9548fc879dc2dc0cb437a98b783581d2b
melqui-andrade <melqui.inbox@gmail.com>
2015-11-26 10:05:46 -0300

    ClienteDomain criado

---
+++ HopeBus/domain/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopeBus.domain
+{
+    class Cliente
+    {
+        int ID { get; set; }
+
+        String Nome { get; set; }
+
+        String Identidade { get; set; }
+
+        String CPF { get; set; }
+
+        String Telefone { get; set; }
+
+        EnumTipo Tipo { get; set; }
+    }
+}
---
+++ HopeBus/domain/EnumTipo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopeBus.domain
+{
+    public enum EnumTipo
+    {
+        Normal = 0,
+        Estudante = 1,
+        Idoso = 2,
+        Deficiente =3
+    }
+}
