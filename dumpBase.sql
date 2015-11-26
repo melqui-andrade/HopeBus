ef841c13fdd6ccbee7583103e595a96e08215968
melqui-andrade <melqui.inbox@gmail.com>
2015-11-26 00:34:54 -0300

    Base de dados criada, dump inicial adicionado ao projeto. [Abrir o arquivo dumpBase.sql, copiar no workbanch mysql e executar a querry]

---
+++ dumpBase.sql
@@ -0,0 +1,184 @@
+﻿-- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hope_bus
+-- ------------------------------------------------------
+-- Server version	5.6.25-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(60) NOT NULL,
+  `cpf` varchar(12) NOT NULL,
+  `identidade` varchar(12) NOT NULL,
+  `telefone` varchar(12) DEFAULT NULL,
+  `tipo` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'Maria Betânia','01734503320','8734518','33214399',1);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente_passagem`
+--
+
+DROP TABLE IF EXISTS `cliente_passagem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente_passagem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL,
+  `id_passagem` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente_passagem`
+--
+
+LOCK TABLES `cliente_passagem` WRITE;
+/*!40000 ALTER TABLE `cliente_passagem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente_passagem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `passagem`
+--
+
+DROP TABLE IF EXISTS `passagem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `passagem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `valor` float NOT NULL,
+  `data` datetime NOT NULL,
+  `poltrona` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `passagem`
+--
+
+LOCK TABLES `passagem` WRITE;
+/*!40000 ALTER TABLE `passagem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `passagem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `passagem_viagem`
+--
+
+DROP TABLE IF EXISTS `passagem_viagem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `passagem_viagem` (
+  `idpassagem_viagem` int(11) NOT NULL,
+  `id_passagem` int(11) NOT NULL,
+  `id_viagem` int(11) NOT NULL,
+  PRIMARY KEY (`idpassagem_viagem`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `passagem_viagem`
+--
+
+LOCK TABLES `passagem_viagem` WRITE;
+/*!40000 ALTER TABLE `passagem_viagem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `passagem_viagem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vendedor`
+--
+
+DROP TABLE IF EXISTS `vendedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vendedor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cep` varchar(10) DEFAULT NULL,
+  `cpf` varchar(12) DEFAULT NULL,
+  `endereco` varchar(80) DEFAULT NULL,
+  `identidade` varchar(12) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vendedor`
+--
+
+LOCK TABLES `vendedor` WRITE;
+/*!40000 ALTER TABLE `vendedor` DISABLE KEYS */;
+INSERT INTO `vendedor` VALUES (1,'58420-120','32123401243','Alameda dos Anjos','4523456','João Mendonça','joao','senha123');
+/*!40000 ALTER TABLE `vendedor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `viagem`
+--
+
+DROP TABLE IF EXISTS `viagem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `viagem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `origem` varchar(45) NOT NULL,
+  `destino` varchar(45) NOT NULL,
+  `horario` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `viagem`
+--
+
+LOCK TABLES `viagem` WRITE;
+/*!40000 ALTER TABLE `viagem` DISABLE KEYS */;
+INSERT INTO `viagem` VALUES (1,'Esperança','Campina Grande','13:00:00'),(2,'Campina Grande','Esperança','13:00:00');
+/*!40000 ALTER TABLE `viagem` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-26  0:19:01
