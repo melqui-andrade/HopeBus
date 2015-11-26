4da6d122143bdda61646420e47322949e6e624c2
melqui-andrade <melqui.inbox@gmail.com>
2015-11-26 09:41:02 -0300

    Classe base do acesso ao banco de dados, criada; Classe do Dominio Vendedor, criada.

---
+++ HopeBus/services/MySqlBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopeBus.services
+{
+    public abstract class MySqlBase
+    {
+        //String de conexão de Melqui
+        public String stringDeConexao = "server=localhost;user id=root;database=hope_bus;password=root";
+
+        //String de conexão de Sidney
+        //public String stringDeConexao = "server=localhost;user id=SEU_ID;database=hope_bus;password=SEU_PASS";
+    }
+}
