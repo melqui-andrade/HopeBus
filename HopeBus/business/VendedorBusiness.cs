61e2bf838f5acdf2b5fa557255e6d9baa3703855
melqui <melqui.andrade@splab.ufcg.edu.br>
2015-12-22 17:53:20 -0300

    Criação do VendedorBusiness

---
+++ HopeBus/business/VendedorBusiness.cs
@@ -0,0 +1,21 @@
+﻿using HopeBus.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopeBus.business
+{
+    class VendedorBusiness
+    {
+        private VendedorMySql _vendedorMySql;
+
+        public bool vendedorAutenticado(String nome, String senha)
+        {
+
+
+            return false;
+        }
+    }
+}
