1746d36a371aefadf4652ed2faf73848b81be6f0
melqui <melqui.andrade@splab.ufcg.edu.br>
2015-12-23 11:15:32 -0300

    ClienteBusiness criado VendedorBusiness finalizado

--- HopeBus/business/VendedorBusiness.cs
+++ HopeBus/business/VendedorBusiness.cs
@@ -1,4 +1,5 @@
 ﻿using HopeBus.services;
+using HopeBus.domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,15 +8,54 @@ using System.Threading.Tasks;
 
 namespace HopeBus.business
 {
-    class VendedorBusiness
+    public class VendedorBusiness
     {
         private VendedorMySql _vendedorMySql;
 
-        public bool vendedorAutenticado(String nome, String senha)
+        public VendedorBusiness()
         {
-
-
-            return false;
+            _vendedorMySql = new VendedorMySql();
         }
+
+        public bool vendedorAutenticado(String nome, String senha)
+        {            
+            return _vendedorMySql.VendedorEstaAutenticado(nome, senha);            
+        }
+
+        public VendedorDomain obtemVendedor(int id)
+        {
+            return _vendedorMySql.ObtemVendedor(id);
+        }
+
+        public List<VendedorDomain> listaVendedores()
+        {            
+            return _vendedorMySql.ObtemVendedores();
+        }
+
+        public List<VendedorDomain> buscaVendedores(String parametro)
+        {
+            return _vendedorMySql.BuscaVendedores(parametro);
+        }
+
+        public void salvaVendedor(String nome, String cpf, String identidade, String login, String senha, String cep, String endereco)
+        {
+            VendedorDomain vendedor = new VendedorDomain();
+            vendedor.Nome = nome;
+            vendedor.CPF = cpf;
+            vendedor.Identidade = identidade;
+            vendedor.Login = login;
+            vendedor.Cep = cep;
+            vendedor.Endereco = endereco;
+            vendedor.Senha = senha;
+
+            _vendedorMySql.SalvaVendedor(vendedor);
+        }        
+
+        public void excluiVendedor(int id)
+        {            
+            _vendedorMySql.ExcluiVendedor(id);
+        }
+
+
     }
 }
